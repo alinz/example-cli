077ff0887cdba07a83ae3df99cac53f054dacc4f
Ali Najafizadeh <a.najafizadeh@gmail.com>
2024-02-21 13:50:19 -0500

    Clean up code

--- main.go
+++ main.go
@@ -28,13 +28,13 @@ func main() {
 func runUpdate() {
 	// In order for selfupdating to work, the following conditions must be met:
 	// 1. Version must be set
-	// 2. SELF_UPDATE_GH_TOKEN must be set
+	// 2. EXAMPLE_CLI_GH_TOKEN must be set
 	// 3. PublicKey must be set
 	// for setting up the token please refer to
 	// "Create a Fine-Grained Personal Access Tokens" in README.md
-	ghToken, ok := os.LookupEnv("SELF_UPDATE_GH_TOKEN")
+	ghToken, ok := os.LookupEnv("EXAMPLE_CLI_GH_TOKEN")
 	if !ok {
-		fmt.Fprintf(os.Stderr, "Warning: SELF_UPDATE_GH_TOKEN env is not set, selfupdating is disabled")
+		fmt.Fprintf(os.Stderr, "Warning: EXAMPLE_CLI_GH_TOKEN env is not set, selfupdating is disabled")
 		return
 	}
 
